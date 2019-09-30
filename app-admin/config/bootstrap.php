4c1452dd3d486024494612de288d0e31729ba8b8
tangbq <tangbq@tiens.com>
2019-09-30 17:29:51 +0800

    ii

--- app-admin/config/bootstrap.php
+++ app-admin/config/bootstrap.php
@@ -1,1 +1,2 @@
 <?php
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/app-admin');
--- app-admin/config/main.php
+++ app-admin/config/main.php
@@ -7,9 +7,9 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
+    'id' => 'app-admin',
     'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'backend\controllers',
+    'controllerNamespace' => 'admin\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
@@ -37,14 +37,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
