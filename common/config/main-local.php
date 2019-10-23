755a964487ec1a99b2063462deb897db00467ac7
tbq <tbq2616@sina.com>
2019-10-23 16:32:40 +0800

    my zhanshen

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=10.0.75.1;dbname=yii-advanced',
+            'dsn' => 'mysql:host=192.168.99.1;dbname=yii-advanced',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
