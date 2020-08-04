a7368f7d2fa64aa0b73f054d8d1494f5a0045858
tbq <tboqi@qq.com>
2020-08-04 14:47:23 +0800

    Cygwin

--- app-restful/modules/v1/controllers/RestfultestController.php
+++ app-restful/modules/v1/controllers/RestfultestController.php
@@ -1,7 +1,6 @@
 <?php
 namespace restful\modules\v1\controllers;
 
-use sizeg\jwt\JwtHttpBearerAuth;
 use yii\rest\ActiveController;
 
 class RestfultestController extends ActiveController
@@ -25,16 +24,16 @@ class RestfultestController extends ActiveController
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-            'class' => JwtHttpBearerAuth::class,
-            'optional' => [
-                'login',
-            ],
-        ];
+    // public function behaviors()
+    // {
+    //     $behaviors = parent::behaviors();
+    //     $behaviors['authenticator'] = [
+    //         'class' => JwtHttpBearerAuth::class,
+    //         'optional' => [
+    //             'login',
+    //         ],
+    //     ];
 
-        return $behaviors;
-    }
+    //     return $behaviors;
+    // }
 }
