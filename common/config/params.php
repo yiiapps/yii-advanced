1f26b37c64df834fc59b803283a5932cdbec003f
tbq <tboqi@qq.com>
2021-04-06 17:48:42 +0800

    iii

--- common/config/params.php
+++ common/config/params.php
@@ -5,5 +5,6 @@ $params = [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
+    'static_website' => '',
 ];
 return yii\helpers\ArrayHelper::merge($params, require $config['vendorPath'] . '/yiiexttbq/blogmodule/configs/params.php');
